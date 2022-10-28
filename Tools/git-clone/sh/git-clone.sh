a4807c4dfcfbfed9d04658bb05c6a23c927168ab
Tekton Pipeline <tekton@pipeline.com>
2022-10-28 10:30:02 +0300

    done with git clone contianer

--- Tools/git-clone/sh/git-clone.sh
+++ Tools/git-clone/sh/git-clone.sh
@@ -3,7 +3,5 @@ echo "Cleaning old files"
 rm -rf ${WORKENV}
 echo "Clone ${GIT} Repository"
 git clone ${GIT} ${WORKENV}
-echo "Pring Local Path"
-pwd
 echo "Show Folder Content" 
-ls -l
+ls -l ${WORKENV}
