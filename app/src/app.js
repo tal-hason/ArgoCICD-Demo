cd04436ba37ac7c57f367e0a6af9686a9b02a918
Tal Hason <97454240+tal-hason@users.noreply.github.com>
2022-11-15 20:16:55 +0200

    starting again

--- app/src/app.js
+++ app/src/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!! Helm test');
+  res.send('Hello World!!');
 });
 
 app.listen(8080, function () {
