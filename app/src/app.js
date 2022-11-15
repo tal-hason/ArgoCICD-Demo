c674bef7b5b63d4133f78450056fbd204b7e69c4
Tekton Pipeline <tekton@pipeline.com>
2022-11-15 16:45:28 +0200

    starting fresh

--- app/src/app.js
+++ app/src/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World! test 2');
+  res.send('Hello World!');
 });
 
 app.listen(8080, function () {
