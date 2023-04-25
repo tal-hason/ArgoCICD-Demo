56b17accd7ad2333b66546090b36f1efbc6b6ede
tal-hason <thason@redhat.com>
2023-04-26 02:03:45 +0300

    added some features to the application

--- app/src/app.js
+++ app/src/app.js
@@ -1,12 +1,16 @@
 var image = process.env.IMAGE
 var tag = process.env.TAG
+var host = process.env.HOSTNAME
 var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send(`Hello new image, My Image is ${image}:${tag} `);
-  console.log('Someone accessed me!')
+  res.send(`Hello new image, My Image is ${image}:${tag} , the Server is ${host} accessed from ${clientHostname} `);
+  var clientHostname = req.headers.host;
+  console.log(`Someone accessed me! from ${clientHostname}`)
 });
+
+
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
